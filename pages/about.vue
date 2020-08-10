7639c15f5702ae75d2fa293bb58537436b0147c0
Benson Njuguna K <bennspencer@gmail.com>
2020-08-10 11:59:09 +0300

    creation of the about us page

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+<p> this is an app that displays corny dad jokes</p>
+  </div>
+</template>
+<script>
+export default {
+  head(){
+    return {
+      title:'About Dad jokes',
+      meta:[
+        {
+          hid: 'description',
+          name:'description',
+          content: 'Best place for Corny dad jokes when taking a coding break.'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
